fac989f7a3c396a65501e82a914234aa6610e47f
XinwenWatson <lixinwe@sheridancollege.ca>
2022-02-04 09:12:46 -0500

    First: ICE1

---
+++ src/ice1/ICE1.java
@@ -0,0 +1,17 @@
+package ice1;
+
+/**
+ * This class +++Insert Description Here+++
+ * 
+ * @author Xinwen Li
+ */
+public class ICE1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
